a7500a344c6d9a6aa9a719727039d7f0088a8d81
lucas-lulu <lucasnoivas4@gmail.com>
2026-02-27 14:29:53 -0300

    up

--- script/script.js
+++ script/script.js
@@ -1,12 +1,18 @@
-
 document.addEventListener('DOMContentLoaded', () => {
     const header = document.getElementById('header');
+    let isScrolling = false;
 
     window.addEventListener('scroll', () => {
-        if (window.scrollY > 50) {
-            header.classList.add('scrolled');
-        } else {
-            header.classList.remove('scrolled');
+        if (!isScrolling) {
+            window.requestAnimationFrame(() => {
+                if (window.scrollY > 50) {
+                    header.classList.add('scrolled');
+                } else {
+                    header.classList.remove('scrolled');
+                }
+                isScrolling = false;
+            });
+            isScrolling = true;
         }
-    });
+    }, { passive: true });
 });
